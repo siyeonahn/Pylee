5f2c33cd767b21f77f07b45a919c69d464b34122
siyeonahn <siyeon0464@naver.com>
2024-10-06 20:17:41 +0900

    pypy

---
+++ PY/1주차/test.py/test.py
@@ -0,0 +1,43 @@
+# print("Hello world")
+
+# a = 1 + 3
+# b = 1 - 3
+# c = 1 * 3
+# d = 1 / 3
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# print(a,b,c,d)
+
+# # a = input()
+# # print(a)
+
+# # b = input()
+# # print(b)
+
+# # print(a,b)
+
+# number1 = int(input())
+# number2 = int(input())
+
+# #int(목적어)-> 숫자로
+# #input() -> 입력받아줘요
+
+# # a +  b = ?
+# print( "a", "+" ,"b", "=" ,"c")
+# n1 = int(input())
+# n2 = int(input())
+# print(n1, "+", n2, "=", n1+n2)
+
+# int()
+# print()
+# input()
+
+# a = input()
+# print(a)
+# print(input())
+
+# 목적어usb = 데이터 -> 결과,숫자,입력값
